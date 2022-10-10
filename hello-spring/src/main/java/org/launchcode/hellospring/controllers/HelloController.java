a7ecdf8d9acc5f827095f0d0d6753ed7bbfe9f06
Natalie Sutton <100653980+nataliersutton@users.noreply.github.com>
2022-10-10 17:02:12 -0500

    Added Query Param and Path Param

--- hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,20 +2,37 @@ package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
-    @GetMapping("hello")
-    @ResponseBody
-    public String hello() {
-        return "Hello, Spring!";
-    }
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello() {
+//        return "Hello, Spring!";
+//    }
 
     @GetMapping("goodbye")
     @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
+    }
+
+    // Create handler to handle request of the form /hello?name=LaunchCode
+
+    @GetMapping("hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!";
+    }
+
+    //Handles requests of the form /hello/LaunchCode
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloWithPathParam(@PathVariable String name){
+        return "Hello, " + name + "!";
     }
 }
