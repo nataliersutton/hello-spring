c1ae91583b51670cbf7232a5460ed454f3c1683d
Natalie Sutton <100653980+nataliersutton@users.noreply.github.com>
2022-10-20 15:55:55 -0500

    Completed static view thymeleaf form

--- hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@ResponseBody
 @RequestMapping("hello")
 public class HelloController {
 
@@ -24,12 +23,14 @@ public class HelloController {
     //now lives at hello/hello
 
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
     public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name + "!";
     }
 
     //Handles requests of the form /hello/LaunchCode
     @GetMapping("{name}")
+    @ResponseBody
     public String helloWithPathParam(@PathVariable String name){
         return "Hello, " + name + "!";
     }
@@ -37,13 +38,6 @@ public class HelloController {
     //now lives at hello/form
     @GetMapping("form")
     public String helloForm(){
-        return "<html>" +
-                "<body>" +
-                "<form action='/hello' method='post'>" + //submit a request to /hello
-                "<input type='text' name='name'>" +
-                "<input type='submit' value='Greet Me!'>" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+        return "form";
     }
 }
